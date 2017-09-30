9f5d18e8f017bd01978f8aa2cbe535878ed47769
Muriel <muriel.mangubat@gmail.com>
2017-09-30 10:13:57 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
-
-	//Letter 
+//Letter choices available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//reset
+//Setting all to zero
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -12,7 +11,7 @@ var letterToGuess = null;
 
 
 
-//the computer select a random letter 
+//Lets the computer select a random letter from the available choices
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 //Allows the user 9 guesses
@@ -45,25 +44,39 @@ updateGuessesLeft();
 
 
 //When key is released it becomes the users guess
-document.onkeypress = function(event) {
-    guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+document.onkeyup = function(event) {
+  // This if condition prevent user from entering other key than letters
+  if(event.which >= 65 && event.which <=90)
+  {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    // This if condition checks whether you already entered letter or not.
+    if(guessedLetters.indexOf(userGuess) < 0)
+    {
+      guessesLeft--;
+      guessedLetters.push(userGuess);
+      updateGuessesLeft();
+      updateGuessesSoFar();
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
-
-        if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                reset();
-            }
-        }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            // Then we'll call the reset. 
-            reset();
+      if (guessesLeft > 0){
+        if (userGuess == letterToGuess){
+          wins++;
+          document.querySelector('#wins').innerHTML = "Wins: " + wins;
+          reset();
         }
-};
+      }else if(guessesLeft == 0){
+      // Then we will loss and we'll update the html to display the loss 
+      losses++;
+      document.querySelector('#losses').innerHTML = "Losses: " + losses;
+      // Then we'll call the reset. 
+      reset();
+      }
+    }
+    else{
+    alert("You already guessed that letter.")
+    }
+
+  }
+  else{
+    alert("Only A-Z Allowd");
+  }
+}
